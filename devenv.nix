0c9ebe977a4fc81971c445a36564f7df2600e0f8
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2026-06-03 03:28:20 +0200

    devenv shell reminder message about the push --force done in CI

--- devenv.nix
+++ devenv.nix
@@ -5,4 +5,11 @@
   languages.javascript.npm.enable = true;
   languages.javascript.npm.install.enable = true;
   dotenv.enable = true;
+  enterShell = ''
+    echo ""
+    echo "Remember to pull force, github does a push force every sunday with the cache"
+    echo ""
+    echo "   git stash && git pull --rebase && git stash pop"
+    echo ""
+  '';
 }
